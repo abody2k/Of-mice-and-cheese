dde3fc0898dd05bab1e09de7a384445eb1b3fb5b
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-03-27 21:56:07 +0300

    functionality to convert harvested seeds into flour, decresaing and increasing numbers, adding main to its own group

--- scripts/bakery.gd
+++ scripts/bakery.gd
@@ -1,6 +1,9 @@
 extends StaticBody3D
 
 
+var seeds_in = 0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,3 +12,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_queue_timeout():
+	if seeds_in > 0:
+		seeds_in -=1
+		get_tree().call_group("main","increase_flour")
+		$queue.start()
