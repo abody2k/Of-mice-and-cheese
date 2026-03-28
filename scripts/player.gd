48c3a637848ad0b11ef1fb5b2a241b96f4e331a2
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-03-28 22:05:48 +0300

    implemented camera modes in player

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,8 +4,20 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+enum MODES {TOP, FPS}
+
+var mode = MODES.TOP
+
 
 func _physics_process(delta):
+	
+	if mode == MODES.TOP:
+		pass
+	else:
+		pass
+		
+	return
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
