c0b97ad40cf07d5bcc5c44a9ccc510931f664d94
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-03-28 21:32:38 +0300

    handled player entering and leaving a pressure point

--- scripts/pressure_point.gd
+++ scripts/pressure_point.gd
@@ -9,3 +9,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_body_exited(body):
+	pass # Replace with function body.
