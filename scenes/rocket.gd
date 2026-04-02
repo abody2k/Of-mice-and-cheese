a63b3c3f68f4d38256469df6d9b306e42e721b31
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-02 23:03:26 +0300

    implemented rocket logic

--- scenes/rocket.gd
+++ scenes/rocket.gd
@@ -1,1 +1,22 @@
 extends CharacterBody3D
+
+
+@export var TIME = 0.5
+
+var target : Vector3 = Vector3(-1,-1,-1)
+
+func compute_xz():
+	target = position + Vector3(100,100,100)
+	velocity.x =(target.x - global_position.x)/TIME
+	velocity.z =(target.z - global_position.z)/TIME
+	velocity.y = ((target.y - global_position.y) + (1.0/2.0 * (-9.8) * pow(TIME,2)))/ TIME
+
+
+func _ready():
+	
+	compute_xz()
+	
+	
+func _process(delta):
+	velocity.y+= delta * -9.8
+	move_and_collide(velocity)
