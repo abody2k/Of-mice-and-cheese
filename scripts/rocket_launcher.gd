6f03002cd1e22f88679950b582b1b92b79c73939
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-02 23:05:03 +0300

    implemented attack logic

--- scripts/rocket_launcher.gd
+++ scripts/rocket_launcher.gd
@@ -1,6 +1,7 @@
 extends "res://scripts/magical_rat.gd"
 
 
+const ROCKET = preload("res://scenes/rocket.tscn")
 func _on_reload_timeout():
 	
 	_attack()
@@ -8,6 +9,6 @@ func _on_reload_timeout():
 
 
 func _attack():
-	
-	
-	pass
+	var rocket = ROCKET.instantiate()
+	rocket.target = global_position+ basis.z * 10
+	add_child(rocket)
