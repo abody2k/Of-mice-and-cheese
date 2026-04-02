96fe734f41b04874d8764551677a0e93c271b3c0
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-02 23:54:08 +0300

    fixed rocket orientation

--- scripts/rocket_launcher.gd
+++ scripts/rocket_launcher.gd
@@ -8,7 +8,7 @@ const ROCKET = preload("res://scenes/rocket.tscn")
 
 func _attack():
 	var rocket = ROCKET.instantiate()
-	rocket.target = global_transform.origin + (-basis.z * 10)
+	rocket.target = global_transform.origin + (-global_transform.basis.z * 10)
 	add_child(rocket)
 	$resting.start()
 	
